bd2e86a55bdb9b182bd4082016afd5d4c13ac26d
yaely20 <y0583248523@gmail.com>
2024-07-03 15:15:55 +0300

    from yiska

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
 
-import express from 'express'  
-console.log("hii")
-const app = express()
+import UsersRouter from "./UsersRouter.js";
+
+console.log("hii");
+const app=express();
+
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/', UsersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
