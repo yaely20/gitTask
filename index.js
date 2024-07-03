a53ecc8ef2db047cb438955ac62f2ca50130ef83
TamarVilner <trviner@gmail.com>
2024-07-03 14:32:13 +0300

    tamarBrunch

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import express from 'express' 
+import bodyParser from "body-parser";
+
 console.log("hii")
+const app = express()
+const port = 3000
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
