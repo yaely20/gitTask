fcdfc5c3b05cf12fc34c2e18466778bd6342453a
yiskaAvramsky <yiska674@gmail.com>
2024-07-03 14:47:41 +0300

    its from yiska branch2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hii")
+import express from "express";
+import bodyParser from "body-parser";
+
+import UsersRouter from "./UsersRouter.js";
+
+console.log("hii");
+const app=express();
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/', UsersRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
